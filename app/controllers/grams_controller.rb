9e2d6e460b218fe8318cacb4403e6ff508d5d399
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-09-08 15:58:29 -0400

    Added destroy action

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -13,7 +13,7 @@ class GramsController < ApplicationController
     return render_not_found if @gram.blank?
 
     @gram.update_attributes(gram_params)
-    
+
     if @gram.valid?
       redirect_to root_path
     else
@@ -38,6 +38,13 @@ class GramsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
   end
 
   private
